5b24d9df8725b2744877006c2f33e28f605fb298
Juan Jose Lopez <53240002+JuanJolh@users.noreply.github.com>
2022-06-18 20:16:30 -0500

    Add files via upload

---
+++ asstets/js/main.js
@@ -0,0 +1,7 @@
+/* Pedir un numero por prompt, repetir la salida del mensaje "Hola" la cantidad de veces ingresada */
+
+let numero = parseInt(prompt("Cuantas veces quiere repetir el mensaje Hola?"))
+
+for (let i=1; i<=numero; i++){
+    alert("hola")
+}
